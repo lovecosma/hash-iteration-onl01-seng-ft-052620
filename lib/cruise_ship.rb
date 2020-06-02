534c815eddaa1e5342002b5ea450f228063b02cd
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-02 20:28:48 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,7 +7,10 @@ suite_e: "Crumpet the Elf"
 }
 
 def select_winner(passengers)
+  winner = ""
 passenger.each do |suite, name|
-    if suite == "suite_a"
+    if suite == "suite_a" &&
+      name.start_with?("A")
+      winner = name
   end
 end
