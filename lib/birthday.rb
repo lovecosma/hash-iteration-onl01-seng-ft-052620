4c397d0d9ad896f4a3fbf0e009872c0324e0cd0d
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-02 20:34:20 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,6 @@ birthday_kids = {
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |name, age|
-    puts age 
+    puts "Happy Birthday #{name}! You are now #{age} years old!""
   end
 end
